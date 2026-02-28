cd3314d84cccc71eb73c379d7d705476f25d11f7
CornWithMint <githubforcorn@gmail.com>
2026-02-28 22:13:00 +0300

    Первый эхо бот

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.25.2
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
 	github.com/go-telegram/bot v1.19.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 )
--- main.go
+++ main.go
@@ -5,44 +5,30 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"syscall"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 )
 
-// обработчик всех сообщений
 func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
-	if update.Message != nil {
-		// Отвечаем тем же текстом (эхо)
-		b.SendMessage(ctx, &bot.SendMessageParams{
-			ChatID: update.Message.Chat.ID,
-			Text:   update.Message.Text,
-		})
-	}
+	b.SendMessage(context.Background(), &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
 }
 
 func main() {
-	// Создаём контекст, который завершится при нажатии Ctrl+C
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 
-	// Получаем токен из переменной окружения
-	token := os.Getenv("TELEGRAM_BOT_TOKEN")
-	if token == "" {
-		log.Fatal("TELEGRAM_BOT_TOKEN environment variable is required")
-	}
-
-	// Настраиваем и создаём бота
 	opts := []bot.Option{
 		bot.WithDefaultHandler(handler),
 	}
 
-	b, err := bot.New(token, opts...)
+	b, err := bot.New(os.Getenv("BOT_TOKEN"), opts...)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Ошибка создания бота")
 	}
-
-	// Запускаем бота (long polling)
 	b.Start(ctx)
+
 }
